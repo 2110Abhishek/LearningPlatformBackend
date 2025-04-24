090bc2ab0d8b5b4b95d374a5eb4f427a45070cd1
2110Abhishek <agchoudhari2110@gmail.com>
2025-04-24 20:15:12 +0530

    env

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve video files
 
 // Connect to MongoDB using environment variable
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log('✅ Connected to MongoDB'))
 .catch(err => console.error('❌ MongoDB connection error:', err));
 
